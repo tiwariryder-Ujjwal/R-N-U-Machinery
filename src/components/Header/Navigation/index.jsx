0e0c49767fe65715ef60df21c14639d1890a2196
tiwariryder-Ujjwal <tiwariryder@gmail.com>
2025-10-07 17:36:20 +0545

    Add Category Panel

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -2,8 +2,13 @@ import Button from "@mui/material/Button";
 import React from "react";
 import { TiThMenu } from "react-icons/ti";
 import {  Link} from "react-router-dom";
+import Categorypanel from "./Categorypanel";
 const Navigation = () => {
     return (
+        <>
+        
+        
+
     <nav>
     <div className="container flex items-center justify-end">
         <div className="col_1 w-[20%]">
@@ -12,28 +17,36 @@ const Navigation = () => {
         <div className="col_2 w-[80%]">
             <ul className="flex items-center gap-7 font-capitalized">
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">Home</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition  !text-blue-950 text-capitalize"><b>HOME</b></Button></Link>
                 </li>
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">Machinery</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>MACHINES</b></Button></Link>
                 </li>
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">Lubricants</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>LUBRICANTS</b></Button></Link>
                 </li>
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]"> Nut/Bolts</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]"> 
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>NUT/BOLTS</b></Button></Link>
                 </li>
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">V-Belts</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>V-BELTS</b></Button></Link>
                 </li>
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">Spanner & Hand Tools</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="!link transition !text-blue-950 text-capitalize"><b>SPANNER & TOOLS</b></Button></Link>
                 </li>
-                
-                
-                
                 <li className="list-none">
-                    <Link to="/" className="link transition text-[15px] font-[500]">Spare Parts</Link>
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>SPARE PARTS</b></Button></Link>
+                </li>
+                <li className="list-none">
+                    <Link to="/" className="link transition text-[15px] font-[500]">
+                    <Button className="link transition !text-blue-950 text-capitalize"><b>OTHERS</b></Button></Link>
                 </li>
             </ul>
             
@@ -41,6 +54,10 @@ const Navigation = () => {
     </div>
     
     </nav>
+
+    <Categorypanel/>
+
+    </>
     )
 }
 
