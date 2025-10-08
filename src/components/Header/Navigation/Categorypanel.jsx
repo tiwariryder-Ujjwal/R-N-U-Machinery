e955729e7de91d004a84ec57f554c5eea6d3e764
tiwariryder-Ujjwal <tiwariryder@gmail.com>
2025-10-08 17:14:41 +0545

    Added Category Panel

--- src/components/Header/Navigation/Categorypanel.jsx
+++ src/components/Header/Navigation/Categorypanel.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-const Categorypanel = () => {
+
+const Categorypanel = (props) => {
+
+const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={props.openCategorypanel(false)}>
+    <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        <ListItem key={text} disablePadding>
+            <ListItemButton>
+            
+            <ListItemText primary={text} />
+            </ListItemButton>
+        </ListItem>
+        ))}
+    </List>
+    <Divider />
+    <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        <ListItem key={text} disablePadding>
+            <ListItemButton>
+            
+            <ListItemText primary={text} />
+            </ListItemButton>
+        </ListItem>
+        ))}
+    </List>
+    </Box>
+    );
     return (
-        <div>Category Panel</div>
+        <>
+        
+    <Drawer open={props.isOpenCatpanel} onClose={props.openCategorypanel(false)}>
+        {DrawerList}
+    </Drawer>
+        </>
     )
 }
 
--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,9 +1,16 @@
 import Button from "@mui/material/Button";
-import React from "react";
+import React, { useState } from "react";
 import { TiThMenu } from "react-icons/ti";
 import {  Link} from "react-router-dom";
 import Categorypanel from "./Categorypanel";
 const Navigation = () => {
+const [isOpenCatpanel ,setIsOpenCatpanel] = useState(false);
+const openCategorypanel=()=>{
+    setIsOpenCatpanel(!isOpenCatpanel);
+}   
+
+
+
     return (
         <>
         
@@ -12,7 +19,7 @@ const Navigation = () => {
     <nav>
     <div className="container flex items-center justify-end">
         <div className="col_1 w-[20%]">
-            <Button className= '!text-black gap-2'> <TiThMenu />  <b> ALL CATEGORIES</b></Button>
+            <Button className= '!text-black gap-2'onClick={openCategorypanel}> <TiThMenu />  <b> ALL CATEGORIES</b></Button>
         </div>
         <div className="col_2 w-[80%]">
             <ul className="flex items-center gap-7 font-capitalized">
@@ -55,7 +62,11 @@ const Navigation = () => {
     
     </nav>
 
-    <Categorypanel/>
+
+
+   
+        {/* category panel component */}
+    <Categorypanel openCategorypanel={openCategorypanel} isOpenCatpanel={isOpenCatpanel}/>
 
     </>
     )
